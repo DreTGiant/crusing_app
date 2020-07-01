07097ba129a4c57f593eccba48e560ad9ca282a7
Andre <andremassey@gmail.com>
2020-07-01 15:02:16 -0700

    update menu-items w/ withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
 import { HomePage } from "./pages/homepage/HomePage.component";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <p>Hello World</p>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={HomePage} />
+          <Route path="/hats" compoent={HatsPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
